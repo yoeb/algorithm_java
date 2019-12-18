d6aca15bbd8e03fd951f5fddfb7be944aec09b24
yangbin <yoeb5930@gmail.com>
2019-12-18 09:06:35 +0800

    静态分派 方法重载优先级

--- src/main/java/com/jvm/chapter_8_3_2/Overload.java
+++ src/main/java/com/jvm/chapter_8_3_2/Overload.java
@@ -2,6 +2,11 @@ package com.jvm.chapter_8_3_2;
 
 import java.io.Serializable;
 
+/**
+ * 重载方法匹配优先级
+ * char -> int -> long -> Character -> Serializable -> Object -> char ...
+ * 演示了编译期间选择静态分派目标的过程，这个过程也是Java语言实现方法重载的本质。
+ */
 public class Overload {
 
     public static void sayHello(Object arg) {
@@ -12,8 +17,16 @@ public class Overload {
         System.out.println("hello int");
     }
 
+    public static void sayHello(int... arg) {
+        System.out.println("hello int ...");
+    }
+
     public static void sayHello(long arg) {
         System.out.println("hello long");
+    }
+
+    public static void sayHello(long... arg) {
+        System.out.println("hello long ...");
     }
 
     public static void sayHello(Character arg) {

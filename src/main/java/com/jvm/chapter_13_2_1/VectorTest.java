a955fedd9506c2c9d8f790ee9ef1daf2c9310bc1
yangbin <yoeb5930@gmail.com>
2019-12-27 09:31:40 +0800

    Vector线程安全测试

---
+++ src/main/java/com/jvm/chapter_13_2_1/VectorTest.java
@@ -0,0 +1,45 @@
+package com.jvm.chapter_13_2_1;
+
+import java.util.Vector;
+
+/**
+ * 对Vector线程安全的测试
+ */
+public class VectorTest {
+
+    private  static Vector<Integer> vector = new Vector<Integer>();
+
+    public static void main(String[] args) {
+        while (true) {
+            for (int i = 0; i < 10; i++) {
+                vector.add(i);
+            }
+
+            Thread removeThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < vector.size(); i++) {
+                        vector.remove(i);
+                    }
+                }
+            });
+
+            Thread printThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < vector.size(); i++) {
+                        System.out.println(vector.get(i));
+                    }
+                }
+            });
+
+            removeThread.start();
+            printThread.start();
+
+            // 不要同时产生过多的线程，否则会导致操作系统假死
+            while (Thread.activeCount() > 20);
+
+        }
+    }
+
+}
